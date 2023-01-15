8ee45b9272c77a6e6d5fb1f59869603512ed94a7
fross72 <fross724@gmail.com>
2023-01-15 13:29:49 +0300

    initial commit

---
+++ src/test/java/com/fross72/demoBlog/DemoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fross72.demoBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
